35e865678fd640a693d1a87cd9731ba66315b18b
Roger Ocker <rocker.jfa@gmail.com>
2025-07-29 19:11:42 -0600

    select function

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def db_create():
                        ''')
         return "B-ball table created"
     except Exception as e:
-        return f"DB error: {e}", 500
+        return f"DB error: {e}", 500 #500 is Internal Server Error
 
 
 @app.route('/db_insert')
@@ -53,3 +53,38 @@ def db_inserting():
         return "Insert successful"
     except Exception as e:
         return f"Insert error: {e}", 500
+
+@app.route('/db_select')
+def db_select():
+    try:
+         with psycopg.connect(DB_URL) as conn:
+            with conn.cursor() as cur:
+                #querry all
+                cur.execute("SELECT * FROM Basketball;")
+                records = cur.fetchall()
+                #column names for table
+                column_names = [desc[0] for desc in cur.description]
+
+        #html
+        response = "<table border = '1'>"
+
+        #header tr=table row th=table header
+        response += 1 "<tr>"
+        for col in column_names:
+            response += f"<th>{col}</th>"
+        response += "</tr>"
+        #rows
+        for row in records:
+            response += "<tr>"
+            for col in row:
+                response += f"<td>{col}</td>"
+            response += "</tr>"
+
+        response += "</table>"
+        return response
+    except Exception as e:
+        return f"Select error: {e}", 500
+        
+
+
+
